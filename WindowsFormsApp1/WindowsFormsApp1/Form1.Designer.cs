bd22a375262bc6202a5cdf62edb17dc71b631778
JMR-2122 <marti20785@iesmarenostrum.com>
2022-02-27 17:33:28 +0100

    Las notas ahora se ponen con un NumericUpdown

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -31,11 +31,12 @@ namespace WindowsFormsApp1
         {
             this.aluNombre = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.aluNota = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.listaAlumnos = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.aluNota = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).BeginInit();
             this.SuspendLayout();
             // 
             // aluNombre
@@ -53,13 +54,6 @@ namespace WindowsFormsApp1
             this.label1.Size = new System.Drawing.Size(135, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Nombre del alumno:";
-            // 
-            // aluNota
-            // 
-            this.aluNota.Location = new System.Drawing.Point(476, 59);
-            this.aluNota.Name = "aluNota";
-            this.aluNota.Size = new System.Drawing.Size(77, 22);
-            this.aluNota.TabIndex = 2;
             // 
             // label2
             // 
@@ -98,20 +92,28 @@ namespace WindowsFormsApp1
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // aluNota
+            // 
+            this.aluNota.Location = new System.Drawing.Point(476, 58);
+            this.aluNota.Name = "aluNota";
+            this.aluNota.Size = new System.Drawing.Size(87, 22);
+            this.aluNota.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.listaAlumnos);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.aluNota);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.aluNombre);
             this.Name = "Form1";
             this.Text = "Alumnos JMR2122";
+            ((System.ComponentModel.ISupportInitialize)(this.aluNota)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,11 +123,11 @@ namespace WindowsFormsApp1
 
         private System.Windows.Forms.TextBox aluNombre;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox aluNota;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox listaAlumnos;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown aluNota;
     }
 }
 
